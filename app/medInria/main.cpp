1e358a51a0aa74b00e171a3a8d1b08341a51d518
Olivier Commowick <olivier.commowick@inria.fr>
2013-12-19 15:38:38 +0100

    Move forceShow so that the main window appears on top of other windows when started from command line

--- app/medInria/main.cpp
+++ app/medInria/main.cpp
@@ -184,8 +184,6 @@ int main(int argc,char* argv[]) {
     if (DirectView)
         mainwindow.setStartup(medMainWindow::WorkSpace,posargs);
 
-    forceShow(mainwindow);
-
     bool fullScreen = medSettingsManager::instance()->value("startup", "fullscreen", false).toBool();
 
     const bool hasFullScreenArg   = application.arguments().contains("--fullscreen");
@@ -238,8 +236,7 @@ int main(int argc,char* argv[]) {
     application.setActivationWindow(&mainwindow);
     application.setMainWindow(&mainwindow);
 
-    mainwindow.raise();
-    mainwindow.activateWindow();
+    forceShow(mainwindow);
 
     //  Start main loop.
 
